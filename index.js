deedcae7dce356136fc1ff68585940c30e821123
Theresa Tran <theresa1194@gmail.com>
2023-09-19 16:31:36 +1000

    solved problems in index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway",
+}
+
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    const newEmployee = { ...employee };
+    newEmployee["name"] = "Sam";
+    newEmployee["streetAddress"] = "11 Broadway";
+    return newEmployee;
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+   employee[key] = value;
+   return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployees = {...employee[key]}
+    delete newEmployees[key];
+    return newEmployees;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
